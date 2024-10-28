c283643344c232011dfa41733c5a17d34d2a37dd
familiachiri2003 <Cristoferapertychiri2003@gmail.com>
2024-10-28 07:48:54 -0600

    Add files via upload

---
+++ calculadora/calculadora.js
@@ -0,0 +1,28 @@
+function suma(){
+    var cris1,cris2,result;
+    cris1=(document.getElementById('cris1').value);
+    cris2=(document.getElementById('cris2').value);
+    result=parseInt(cris1)+parseInt(cris2);
+    titulo.innerHTML="la respuesta es:"+result;
+}
+function resta(){
+    var chiri3,chiri4,result;
+    chiri3=(document.getElementById('chiri3').value);
+    chiri4=(document.getElementById('chiri4').value);
+    result=parseInt(chiri3)-parseInt(chiri4);
+    gladys.innerHTML="la respuesta es:"+result;
+}
+function multiplicacion(){
+    var ali5,ali6,result;
+    ali5=(document.getElementById('ali5').value);
+    ali6=(document.getElementById('ali6').value);
+    result=parseInt(ali5)*parseInt(ali6);
+    alisson.innerHTML="la respuesta es:"+result;
+}
+function division(){
+    var gus7,gus8,result;
+    gus7=(document.getElementById('gus7').value);
+    gus8=(document.getElementById('gus8').value);
+    result=parseInt(gus7)/parseInt(gus8);
+    angulo.innerHTML="la respuesta es:"+result;
+}
